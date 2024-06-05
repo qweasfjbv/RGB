49a2b86c16e44570a9c0eef4958b1c0e30407814
Inho <yeoinho75@gmail.com>
2024-06-05 20:11:50 +0900

    refactor: MoveCube by func call
    - add prevInputBuffer

--- Assets/0_Script/BoxController.cs
+++ Assets/0_Script/BoxController.cs
@@ -1,35 +1,52 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
-[Serializable]
 public enum BoxDir{FORWARD = 0, BOTTOM, BACK, TOP, LEFT, RIGHT}
 
 public class BoxController : MonoBehaviour
 {
-
+    [Header("Jump Variables")]
     [SerializeField] private float jumpDistance = 1.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float jumpDuration = 0.3f;
-    [SerializeField] private BoxDir[] boxDirs;
+    [Space(20)]
 
+    [Header("Input Buffer")]
+    [SerializeField, Range(0f, 1f),] private float inputThreshold;
+    [Space(20)]
+
+    [Header("DEBUG")]
+    [SerializeField] private BoxDir[] boxDirs;
+    [Space(20)]
+
+    // Index sequence to rotate boxDirs
     readonly int[] vertIdx = new int[4] { 3, 0, 1, 2 };
     readonly int[] horzIdx = new int[4] { 3, 5, 1, 4 };
+    readonly KeyCode[] arrowKeys = { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow };
 
-    private bool isJumping = false;
+
+    private float jumpProgress = 0f;
+    private bool isJumping;
     private Vector3 jumpStart;
     private Vector3 jumpTarget;
     private Vector3 rotateAxis;
+    private Vector3 direction;
     private Quaternion targetRotation;
     private Quaternion startRotation;
-    
-    private float jumpStartTime;
 
+    // Previous input buffer
+    private KeyCode prevInputBuffer;
 
     private void Start()
     {
         boxDirs = new BoxDir[6] { BoxDir.FORWARD, BoxDir.BOTTOM, BoxDir.BACK, BoxDir.TOP, BoxDir.LEFT, BoxDir.RIGHT };
+
+        direction = Vector3.zero;
+        prevInputBuffer = KeyCode.None;
+        isJumping = false;
     }
 
     private void RotateBox(BoxDir rotateDir)
@@ -37,7 +54,7 @@ public class BoxController : MonoBehaviour
         // Error Control
         if (rotateDir == BoxDir.TOP || rotateDir == BoxDir.BOTTOM)
         {
-            Debug.Log("ERROR : BOXROTATE GET WRONG INPUT"); return;
+            Debug.LogError("ERROR : BOXROTATE GET WRONG INPUT"); return;
         }
 
         // Direction Update
@@ -81,68 +98,100 @@ public class BoxController : MonoBehaviour
 
     void Update()
     {
-        if (isJumping)  // Jump in progress
+
+        foreach (KeyCode key in arrowKeys)
         {
-            float progress = (Time.time - jumpStartTime) / jumpDuration;
-            if (progress < 1.0f)
+            if (Input.GetKey(key))
             {
-                // Calculate parabola
-                float height = Mathf.Sin(Mathf.PI * progress) * jumpHeight;
-                transform.position = Vector3.Lerp(jumpStart, jumpTarget, progress) + new Vector3(0, height, 0);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, progress);
-            }
-            else
-            {
-                // Jump Complete
-                transform.position = jumpTarget;
-                transform.rotation = targetRotation;
-                isJumping = false;
+                GetKeyInput(key); return;
             }
         }
-        else
-        {
-            // Get Input Event
 
-            Vector3 direction = Vector3.zero;
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
+    }
+
+
+    private void GetKeyInput(KeyCode key)
+    {
+        // Key Block during Jumping
+        if (isJumping)
+        {
+            if (jumpProgress > inputThreshold) prevInputBuffer = key;
+            return;
+        }
+
+        prevInputBuffer = KeyCode.None;
+        direction = Vector3.zero;
+
+        switch (key) {
+            case KeyCode.UpArrow:
                 direction = Vector3.forward;
                 RotateBox(BoxDir.FORWARD);
                 rotateAxis = new Vector3(1, 0, 0);
+                break;
 
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
+            case KeyCode.DownArrow:
                 direction = Vector3.back;
-                RotateBox(BoxDir.FORWARD);
+                RotateBox(BoxDir.BACK);
                 rotateAxis = new Vector3(-1, 0, 0);
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
+                break;
+
+            case KeyCode.LeftArrow:
                 direction = Vector3.left;
-                RotateBox(BoxDir.FORWARD);
+                RotateBox(BoxDir.LEFT);
                 rotateAxis = new Vector3(0, 0, 1);
+                break;
 
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
+            case KeyCode.RightArrow:
                 direction = Vector3.right;
-                RotateBox(BoxDir.FORWARD);
+                RotateBox(BoxDir.RIGHT);
                 rotateAxis = new Vector3(0, 0, -1);
-            }
-
-
-            if (direction != Vector3.zero)
-            {
-                // Jump Start
-                jumpStart = transform.position;
-                jumpTarget = jumpStart + direction * jumpDistance;
-                jumpTarget.y = jumpStart.y;  // y 좌표는 동일하게 유지
-                startRotation = transform.rotation;
-                targetRotation = Quaternion.AngleAxis(90, rotateAxis) * startRotation;
-                jumpStartTime = Time.time;
-                isJumping = true;
-            }
+                break;
         }
+
+
+        if (direction != Vector3.zero)
+        {
+            // Jump Start
+            jumpStart = transform.position;
+            jumpTarget = jumpStart + direction * jumpDistance;
+            jumpTarget.y = jumpStart.y;
+
+            startRotation = transform.rotation;
+            targetRotation = Quaternion.AngleAxis(90, rotateAxis) * startRotation;
+
+            StartCoroutine(JumpCoroutine(jumpDuration));
+        }
+
+
+        return;
     }
+
+    private IEnumerator JumpCoroutine(float duration)
+    {
+        jumpProgress = 0f;
+        isJumping = true;
+        float elapsedTime = 0f;
+
+        while (jumpProgress < 1.0f)
+        {
+            jumpProgress = elapsedTime / duration;
+            // Calculate parabola
+            float height = Mathf.Sin(Mathf.PI * jumpProgress) * jumpHeight;
+            transform.position = Vector3.Lerp(jumpStart, jumpTarget, jumpProgress) + new Vector3(0, height, 0);
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, jumpProgress);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Jump Complete
+        transform.position = jumpTarget;
+        transform.rotation = targetRotation;
+        isJumping = false;
+
+        // If inputBuffer exist -> direct execute
+        if (prevInputBuffer != KeyCode.None) GetKeyInput(prevInputBuffer);
+    }
+
+
 }
