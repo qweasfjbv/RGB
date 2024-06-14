b9f7501fb3c4aed5f3c68f7ab34c85097e96caaa
Inho <yeoinho75@gmail.com>
2024-06-14 22:10:38 +0900

    fix: color code

--- Assets/0_Script/5_Util/Constant.cs
+++ Assets/0_Script/5_Util/Constant.cs
@@ -12,11 +12,11 @@ public static class ColorConstants {
 
     // TODO : Color Modify Needed
     public static readonly Color RED = new Color(255f/255, 50f/255, 50f/255, 1f);
-    public static readonly Color BLUE = new Color(1f, 0f, 0f, 1f);
-    public static readonly Color GREEN = new Color(1f, 0f, 0f, 1f);
-    public static readonly Color YELLOW = new Color(1f, 0f, 0f, 1f);
-    public static readonly Color CYAN = new Color(1f, 0f, 0f, 1f);
-    public static readonly Color MAGENTA = new Color(1f, 0f, 0f, 1f);
+    public static readonly Color BLUE = new Color(50f / 255, 50f / 255, 255f / 255, 1f);
+    public static readonly Color GREEN = new Color(50f / 255, 255f / 255, 255f / 255, 1f);
+    public static readonly Color YELLOW = new Color(255f / 255, 255f / 255, 30f / 255, 1f);
+    public static readonly Color CYAN = new Color(30f / 255, 255f / 255, 255f / 255, 1f);
+    public static readonly Color MAGENTA = new Color(255f / 255, 30f / 255, 255f / 255, 1f);
     public static readonly Color BLACK = new Color(0f, 0f, 0f, 1f);
     public static readonly Color WHITE = new Color(1f, 1f, 1f, 1f);
 }
